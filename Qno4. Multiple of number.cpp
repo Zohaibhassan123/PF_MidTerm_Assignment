a5a02188a076b6f8e2147900acae06e6a9bac665
Zohaibhassan123 <zh279244@gmail.com>
2025-12-02 22:51:15 +0500

    Create Qno4. Multiple of number.cpp

---
+++ Qno4. Multiple of number.cpp
@@ -0,0 +1,17 @@
+Solution:
+
+#include <iostream>
+using namespace std;
+int main() {
+int num1, num2;
+cout << "Enter first number: ";
+cin >> num1;
+cout << "Enter second number: ";
+cin >> num2:
+if (num1% num2 == 0) {
+cout << num2 << " is a multiple of " << num1;
+else {
+}
+cout << num2 << " is NOT a multiple of " << num1;
+return 0;
+}
